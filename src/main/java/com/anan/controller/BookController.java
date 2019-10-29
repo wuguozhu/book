d6f9c1185295f4804ec02a17f6b5d725059787ce
wuguozhu <wugzhu@outlook.com>
2019-10-29 23:13:50 +0800

    add book server and controller

--- src/main/java/com/anan/controller/BookController.java
+++ src/main/java/com/anan/controller/BookController.java
@@ -1,11 +1,13 @@
 package com.anan.controller;
 
 
+import com.anan.entity.Book;
 import com.anan.service.impl.BookServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/book")
@@ -15,9 +17,50 @@ public class BookController {
     @Autowired
     private BookServiceImpl bookService;
 
-    @RequestMapping(value = "/getBookById/{id}")
-    public String getBookById(@PathVariable Integer id) {
-        return bookService.getBookById(id).toString();
+    /**
+     * 添加图书
+     * @param book 图书实例
+     * @return 成功状态
+     */
+    @PostMapping(path = "/saveBook")
+    public ResponseEntity<?> saveBook(@RequestBody Book book){
+        return new ResponseEntity<>(bookService.saveBook(book),
+                HttpStatus.OK);
+    }
 
+    /**\
+     * 下架图书
+     * @param id 图书id
+     * @return 成功状态
+     */
+
+    @DeleteMapping(path = "/deleteBookById/{id}")
+    public ResponseEntity<?> deleteBookById(@PathVariable String id){
+        return new ResponseEntity<>(bookService.deleteBookById(Integer.parseInt(id)),
+                HttpStatus.OK);
+    }
+
+
+    /**
+     * 根据id更新图书
+     * @param book 图书实体
+     * @return 成功状态
+     */
+    @PutMapping(path = "/updateBook")
+    public ResponseEntity<?> updateBook( Book book){
+        return new ResponseEntity<>(bookService.updateBook(book),
+                HttpStatus.OK);
+    }
+
+    /**
+     * 根据id查询图书
+     * @param id 图书id
+     * @return 成功状态
+     */
+    @RequestMapping(path = "/getBookById/{id}")
+    public ResponseEntity<?> getBookById(@PathVariable String id) {
+        return new ResponseEntity<>(
+                bookService.getBookById(Integer.parseInt(id)),
+                HttpStatus.OK);
     }
 }
--- src/test/java/com/anan/service/impl/BookServiceImplTest.java
+++ src/test/java/com/anan/service/impl/BookServiceImplTest.java
@@ -1,8 +1,12 @@
 package com.anan.service.impl;
 
 import com.anan.BookApplication;
+import com.anan.entity.Book;
+import com.anan.service.BookService;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -12,19 +16,48 @@ import static org.junit.Assert.*;
 @SpringBootTest(classes = BookApplication.class)
 public class BookServiceImplTest {
 
+
+    @Autowired
+    private BookService bookService;
+
     @Test
     public void getBookById() {
+        Book book = bookService.getBookById(11);
+        System.out.println(book.toString());
     }
 
     @Test
     public void saveBook() {
+        Book book = new Book();
+        book.setId(4);
+        book.setName("重构-改善既有代码的设计1");
+        book.setNumber(101);
+        book.setPrice(69);
+        book.setPrintshop("人民邮电出版社");
+
+        int i = bookService.saveBook(book);
+        Assert.assertFalse(i < 0);
+        Assert.assertEquals("返回错误", 1, i);
     }
 
     @Test
     public void deleteBookById() {
+        int result = bookService.deleteBookById(4);
+        Assert.assertFalse(result < 0);
+        Assert.assertEquals("返回错误", 0, result);
     }
 
     @Test
     public void updateBook() {
+        Book book = new Book();
+        book.setId(11);
+        book.setName("重构-改善既有代码的设计");
+        book.setNumber(100);
+        book.setPrice(69);
+        book.setPrintshop("人民邮电出版社");
+
+        int i = bookService.updateBook(book);
+        Assert.assertFalse(i < 0);
+        Assert.assertEquals("返回错误", 1, i);
     }
 }
